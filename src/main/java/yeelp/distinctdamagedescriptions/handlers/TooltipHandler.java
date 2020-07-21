d2eea93380d488a81d331b52d0eb4c4e09bd580f
Duncan McKay <eliteluigi38@gmail.com>
2020-07-21 19:51:15 -0400

    update tooltips

--- src/main/java/yeelp/distinctdamagedescriptions/handlers/TooltipHandler.java
+++ src/main/java/yeelp/distinctdamagedescriptions/handlers/TooltipHandler.java
@@ -6,6 +6,7 @@ import java.util.List;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemMonsterPlacer;
+import net.minecraft.util.Tuple;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentTranslation;
@@ -27,22 +28,29 @@ import yeelp.distinctdamagedescriptions.util.MobResistanceCategories;
 public class TooltipHandler extends Handler
 {
 	private static final DecimalFormat formatter = new DecimalFormat("##.##%");
-	private static ITextComponent slashTooltip = new TextComponentTranslation("damagetypes.distinctdamagedescriptions.slashing").setStyle(new Style().setColor(TextFormatting.WHITE));
-	private static ITextComponent pierceTooltip = new TextComponentTranslation("damagetypes.distinctdamagedescriptions.piercing").setStyle(new Style().setColor(TextFormatting.WHITE));
-	private static ITextComponent bludgeTooltip = new TextComponentTranslation("damagetypes.distinctdamagedescriptions.bludgeoning").setStyle(new Style().setColor(TextFormatting.WHITE));
-	private static ITextComponent shiftTooltip = new TextComponentTranslation("keys.distinctdamagedescriptions.shift").setStyle(new Style().setColor(TextFormatting.YELLOW));
-	private static ITextComponent ctrlTooltip = new TextComponentTranslation("keys.distinctdamagedescriptions.ctrl").setStyle(new Style().setColor(TextFormatting.YELLOW));
-	private static ITextComponent damageTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.damage").setStyle(new Style().setColor(TextFormatting.GRAY));
-	private static ITextComponent resistanceTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.resistance").setStyle(new Style().setColor(TextFormatting.GRAY));
-	private static ITextComponent weaknessTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.weakness").setStyle(new Style().setColor(TextFormatting.DARK_RED)); 
-	private static ITextComponent effectivenessTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.effectiveness").setStyle(new Style().setColor(TextFormatting.GRAY));
-	private static ITextComponent damageDistTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.damagedistribution").setStyle(new Style().setColor(TextFormatting.GRAY));
-	private static ITextComponent projDistTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.projectiledistribution").setStyle(new Style().setColor(TextFormatting.GRAY));
-	private static ITextComponent armorResistTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.armorresistances").setStyle(new Style().setColor(TextFormatting.GRAY));
-	private static ITextComponent mobResistTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.mobresistances").setStyle(new Style().setColor(TextFormatting.GRAY));
-	private static ITextComponent mobImmunityTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.immunities").setStyle(new Style().setColor(TextFormatting.AQUA));
-	private static ITextComponent mobAdaptiveTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.adaptivechance").setStyle(new Style().setColor(TextFormatting.LIGHT_PURPLE));
-	private static ITextComponent[] damageTypeTooltips = {slashTooltip, pierceTooltip, bludgeTooltip};
+	private static final Style GRAY = new Style().setColor(TextFormatting.GRAY);
+	private static final Style WHITE = new Style().setColor(TextFormatting.WHITE);
+	private static final Style LIGHT_PURPLE = new Style().setColor(TextFormatting.LIGHT_PURPLE);
+	private static final Style YELLOW = new Style().setColor(TextFormatting.YELLOW);
+	private static final Style AQUA = new Style().setColor(TextFormatting.AQUA);
+	private static final Style DARK_RED = new Style().setColor(TextFormatting.DARK_RED);
+	private static final ITextComponent slashTooltip = new TextComponentTranslation("damagetypes.distinctdamagedescriptions.slashing").setStyle(WHITE);
+	private static final ITextComponent pierceTooltip = new TextComponentTranslation("damagetypes.distinctdamagedescriptions.piercing").setStyle(WHITE);
+	private static final ITextComponent bludgeTooltip = new TextComponentTranslation("damagetypes.distinctdamagedescriptions.bludgeoning").setStyle(WHITE);
+	private static final ITextComponent shiftTooltip = new TextComponentTranslation("keys.distinctdamagedescriptions.shift").setStyle(YELLOW);
+	private static final ITextComponent ctrlTooltip = new TextComponentTranslation("keys.distinctdamagedescriptions.ctrl").setStyle(YELLOW);
+	private static final ITextComponent damageTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.damage").setStyle(GRAY);
+	private static final ITextComponent resistanceTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.resistance").setStyle(GRAY);
+	private static final ITextComponent weaknessTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.weakness").setStyle(DARK_RED); 
+	private static final ITextComponent effectivenessTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.effectiveness").setStyle(GRAY);
+	private static final ITextComponent damageDistTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.damagedistribution").setStyle(GRAY);
+	private static final ITextComponent projDistTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.projectiledistribution").setStyle(GRAY);
+	private static final ITextComponent armorResistTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.armorresistances").setStyle(GRAY);
+	private static final ITextComponent mobResistTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.mobresistances").setStyle(GRAY);
+	private static final ITextComponent mobImmunityTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.immunities").setStyle(AQUA);
+	private static final ITextComponent mobAdaptiveTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.adaptivechance").setStyle(LIGHT_PURPLE);
+	private static final ITextComponent mobAdaptiveAmountTooltip = new TextComponentTranslation("tooltips.distinctdamagedescriptions.adaptiveAmount").setStyle(LIGHT_PURPLE);
+	private static final ITextComponent[] damageTypeTooltips = {slashTooltip, pierceTooltip, bludgeTooltip};
 	private static final String resistanceColor = TextFormatting.GRAY.toString();
 	private static final String weaknessColor = TextFormatting.DARK_RED.toString();
 	
@@ -112,6 +120,8 @@ public class TooltipHandler extends Handler
 				float[] resistsPercents = {mobCats.getSlashingResistance(), mobCats.getPiercingResistance(), mobCats.getBludgeoningResistance()};
 				boolean[] immunities = {mobCats.getSlashingImmunity(), mobCats.getPiercingImmunity(), mobCats.getBludgeoningImmunity()};
 				float adaptive = mobCats.adaptiveChance();
+				float adaptiveAmount = mobCats.getAdaptiveAmount();
+				Tuple<Float, Float> adaptiveInfo = new Tuple<Float, Float>(adaptive, adaptiveAmount);
 				boolean hasImmunities = false;
 				for(DamageType type : DamageType.values())
 				{
@@ -134,7 +144,9 @@ public class TooltipHandler extends Handler
 					{
 						index = tooltips.size()-2;
 					}
-					tooltips.add(index, makeMobAdaptiveTooltip(adaptive));
+					Tuple<String, String> adaptiveTooltips = makeMobAdaptiveTooltip(adaptiveInfo);
+					tooltips.add(index, adaptiveTooltips.getFirst());
+					tooltips.add(index, adaptiveTooltips.getSecond());
 				}
 			}
 			tooltips.add(startingIndex, mobResistTooltip.getFormattedText() + (ctrlHeld ? "" : " "+ctrlTooltip.getFormattedText()));
@@ -164,23 +176,23 @@ public class TooltipHandler extends Handler
 		}
 	}
 
-	private String makeArmorTooltip(float percent, ITextComponent tooltip)
+	private static String makeArmorTooltip(float percent, ITextComponent tooltip)
 	{
 		return String.format("   %s %s %s", formatNum(percent).substring(1), tooltip.getFormattedText(), effectivenessTooltip.getFormattedText());
 	}
 
-	private String makeDamagePercentTooltip(float percent, ITextComponent tooltip)
+	private static String makeDamagePercentTooltip(float percent, ITextComponent tooltip)
 	{
 		return String.format("   %s %s %s", formatNum(percent).substring(1), tooltip.getFormattedText(), damageTooltip.getFormattedText());
 	}
 	
-	private String makeMobResistTooltip(float percent, ITextComponent tooltip)
+	private static String makeMobResistTooltip(float percent, ITextComponent tooltip)
 	{
 		boolean isNegative = percent < 0;
 		return String.format("   %s%s %s %s", isNegative ? weaknessColor : resistanceColor, formatNum(percent).substring(1), tooltip.getFormattedText(), isNegative ? weaknessTooltip.getFormattedText() : resistanceTooltip.getFormattedText());
 	}
 	
-	private String makeMobImmunityTooltip(boolean[] immunities)
+	private static String makeMobImmunityTooltip(boolean[] immunities)
 	{
 		String str = mobImmunityTooltip.getFormattedText()+" ";
 		for(DamageType type : DamageType.values())
@@ -194,12 +206,14 @@ public class TooltipHandler extends Handler
 		return str.substring(0, str.length()-1);
 	}
 	
-	private String makeMobAdaptiveTooltip(float percent)
+	private static Tuple<String, String> makeMobAdaptiveTooltip(Tuple<Float, Float> adaptiveInfo)
 	{
-		return String.format("%s %s", mobAdaptiveTooltip.getFormattedText(), formatNum(percent).substring(1));
+		String str1 = String.format("%s %s", mobAdaptiveTooltip.getFormattedText(), formatNum(adaptiveInfo.getFirst().floatValue()).substring(1));
+		String str2 = String.format("   %s %s", mobAdaptiveAmountTooltip.getFormattedText(), formatNum(adaptiveInfo.getSecond().floatValue()).substring(1));
+		return new Tuple<String, String>(str1, str2);
 	}
 	
-	private String formatNum(float num)
+	private static String formatNum(float num)
 	{
 		return String.format("%s%s", num < 0 ? "" : "+", formatter.format(num));
 	}
