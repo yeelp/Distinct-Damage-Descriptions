ce16ee40c71654f42b429abf60097900537b0603
Duncan McKay <eliteluigi38@gmail.com>
2020-08-21 09:12:09 -0400

    Version bump

--- src/main/java/yeelp/distinctdamagedescriptions/ModConsts.java
+++ src/main/java/yeelp/distinctdamagedescriptions/ModConsts.java
@@ -4,5 +4,5 @@ public class ModConsts
 {
 	public static final String MODID = "distinctdamagedescriptions";
 	public static final String NAME = "Distinct Damage Descriptions";
-	public static final String VERSION = "1.0.0-alpha";
+	public static final String VERSION = "1.0.0-beta";
 }
