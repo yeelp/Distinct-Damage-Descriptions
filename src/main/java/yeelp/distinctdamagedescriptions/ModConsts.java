1afc522cbc364cfe331452612d26ef1b3f9e0fdc
Yeelp <eliteluigi38@gmail.com>
2020-12-24 17:36:32 -0500

    Version bump

--- src/main/java/yeelp/distinctdamagedescriptions/ModConsts.java
+++ src/main/java/yeelp/distinctdamagedescriptions/ModConsts.java
@@ -5,7 +5,7 @@ public class ModConsts
 	public static final String MODID = "distinctdamagedescriptions";
 	public static final String MODID_SHORT = "ddd";
 	public static final String NAME = "Distinct Damage Descriptions";
-	public static final String VERSION = "1.1.0-beta";
+	public static final String VERSION = "1.2.0-beta";
 	
 	public static final String CRAFTTWEAKER_ID = "crafttweaker";
 	public static final class InternalDamageTypes
