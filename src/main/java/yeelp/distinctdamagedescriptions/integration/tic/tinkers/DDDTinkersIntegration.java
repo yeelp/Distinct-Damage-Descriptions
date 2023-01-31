c6c98b7051d83a193844f28bca8bced6571f9589
yeelp <eliteluigi38@gmail.com>
2023-01-31 11:49:02 -0500

    Fix #88, Let DDD re-register NBT specific melting receipes

--- src/main/java/yeelp/distinctdamagedescriptions/integration/tic/tinkers/DDDTinkersIntegration.java
+++ src/main/java/yeelp/distinctdamagedescriptions/integration/tic/tinkers/DDDTinkersIntegration.java
@@ -1,6 +1,7 @@
 package yeelp.distinctdamagedescriptions.integration.tic.tinkers;
 
 import java.lang.reflect.Field;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.google.common.collect.ImmutableList;
@@ -10,9 +11,16 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import slimeknights.mantle.util.RecipeMatch;
+import slimeknights.mantle.util.RecipeMatch.ItemCombination;
+import slimeknights.tconstruct.TConstruct;
+import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.events.TinkerRegisterEvent.MeltingRegisterEvent;
 import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.library.smeltery.MeltingRecipe;
 import slimeknights.tconstruct.tools.ranged.TinkerRangedWeapons;
 import yeelp.distinctdamagedescriptions.DistinctDamageDescriptions;
 import yeelp.distinctdamagedescriptions.ModConsts;
@@ -39,6 +47,33 @@ public class DDDTinkersIntegration extends DDDTiCIntegration {
 	private static final Class<?> TINKER_REGISTRY;
 	public static final Modifier slyStrike = new ModifierSlyStrike(), bruteForce = new ModifierBruteForce();
 
+	protected static final class MeltingListener extends Handler {
+
+		private static MeltingListener instance;
+
+		private final List<MeltingRecipe> recipesToFix;
+
+		private MeltingListener() {
+			this.recipesToFix = new LinkedList<MeltingRecipe>();
+		}
+
+		@SubscribeEvent(priority = EventPriority.LOWEST)
+		public void onMeltingRegister(MeltingRegisterEvent evt) {
+			// The only NBT-sensitive recipe we need to edit. Leave the others.
+			if(evt.getRecipe().input instanceof ItemCombination) {
+				this.recipesToFix.add(evt.getRecipe());
+			}
+		}
+
+		public void fixRecipes() {
+			this.recipesToFix.forEach((mr) -> TinkerRegistry.registerMelting(new MeltingRecipe(new RecipeMatch.ItemCombination(mr.input.amountMatched, mr.input.getInputs().get(0).copy()), mr.output, mr.temperature)));
+		}
+
+		static MeltingListener getInstance() {
+			return instance == null ? instance = new DDDTinkersIntegration.MeltingListener() : instance;
+		}
+	}
+
 	static {
 		try {
 			TINKER_REGISTRY = Class.forName("slimeknights.tconstruct.library.TinkerRegistry");
@@ -55,10 +90,11 @@ public class DDDTinkersIntegration extends DDDTiCIntegration {
 		}
 		catch(SecurityException e) {
 			DistinctDamageDescriptions.fatal("Some security problem occured initializing Tinker's integration.");
-			throw new RuntimeException(e);			
+			throw new RuntimeException(e);
 		}
+		MeltingListener.getInstance().register();
 	}
-	
+
 	@Override
 	public boolean doSpecificInit(FMLInitializationEvent evt) {
 		slyStrike.addRecipeMatch(new RecipeMatch.ItemCombination(1, new ItemStack(Items.GHAST_TEAR), new ItemStack(Items.COMPASS), new ItemStack(Items.ENDER_EYE)));
@@ -69,21 +105,25 @@ public class DDDTinkersIntegration extends DDDTiCIntegration {
 
 	@Override
 	public boolean postInit(FMLPostInitializationEvent evt) {
-		for(Item i : ImmutableList.of(TinkerRangedWeapons.arrow, TinkerRangedWeapons.bolt, TinkerRangedWeapons.shuriken)) {
-			String s = ForgeRegistries.ITEMS.getKey(i).toString();
-			DDDConfigurations.projectiles.registerItemProjectilePair(s, s);
-			DDDConfigurations.projectiles.put(s, new DamageDistribution());
+		if(TConstruct.pulseManager.isPulseLoaded(TinkerRangedWeapons.PulseId)) {
+			for(Item i : ImmutableList.of(TinkerRangedWeapons.arrow, TinkerRangedWeapons.bolt, TinkerRangedWeapons.shuriken)) {
+				String s = ForgeRegistries.ITEMS.getKey(i).toString();
+				DDDConfigurations.projectiles.registerItemProjectilePair(s, s);
+				DDDConfigurations.projectiles.put(s, new DamageDistribution());
+			}
 		}
-		//Add DDD specific information to Tinker's registry.
+
+		// Add DDD specific information to Tinker's registry.
 		try {
 			@SuppressWarnings("unchecked")
-			List<ItemStack> registry = (List<ItemStack>)TINKER_STENCIL_REGISTRY.get(null);
-			registry.replaceAll(ItemStack::copy);
+			List<ItemStack> stencilRegistry = (List<ItemStack>) TINKER_STENCIL_REGISTRY.get(null);
+			stencilRegistry.replaceAll(ItemStack::copy);
 		}
-		catch (IllegalAccessException | IllegalArgumentException e) {
+		catch(IllegalAccessException | IllegalArgumentException e) {
 			DistinctDamageDescriptions.fatal("Could not get underlying stencil registry!");
 			throw new RuntimeException(e);
 		}
+		MeltingListener.getInstance().fixRecipes();
 		DDDRegistries.trackers.register(new BattleSignTracker());
 		DDDRegistries.distributions.register(new BattleSignCounterAttackDistribution());
 		return true;
