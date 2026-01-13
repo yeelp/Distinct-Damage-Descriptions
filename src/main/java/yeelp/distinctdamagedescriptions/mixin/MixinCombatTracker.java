7f7d2dcfcc7af3b9628f834a83f3ec0d95f1d7ff
viciscat <51047087+viciscat@users.noreply.github.com>
2026-01-13 20:39:53 +0100

    Use Shadow instead of Accessor (#131)

--- src/main/java/yeelp/distinctdamagedescriptions/mixin/MixinCombatTracker.java
+++ src/main/java/yeelp/distinctdamagedescriptions/mixin/MixinCombatTracker.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,8 +21,8 @@ import yeelp.distinctdamagedescriptions.util.lib.DebugLib;
 @Mixin(CombatTracker.class)
 public abstract class MixinCombatTracker {
 
-	@Accessor("fighter")
-	protected abstract EntityLivingBase getFighter();
+	@Shadow
+	public abstract EntityLivingBase getFighter();
 	
 	@Unique
 	private static final int TICK_DELTA = 50;
